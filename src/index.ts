a92f3741af3bbc1fcaeb47aa5ae899bc1ec998bc
Saurabh Lohiya <saurabhlohiya1234@gmail.com>
2024-01-21 00:19:14 +0530

    Making Code Cleaner and following OOP Principles

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,45 @@
 import { ThreeScene } from "./initScene"
 import * as dat from "dat.gui"
 
+export enum Mode {
+	Draw = "draw",
+	Move = "move",
+	EditVertex = "editVertex",
+	Extrude = "extrude",
+}
+interface IEnumMode {
+	[key: string]: Mode
+}
+const editMode: IEnumMode = {
+	Draw: Mode.Draw,
+	Move: Mode.Move,
+	["Edit Verted"]: Mode.EditVertex,
+	Extrude: Mode.Extrude,
+}
 const threeScene = new ThreeScene()
+
 const gui = new dat.GUI()
+let modeController = gui.add(threeScene, "mode", Object.keys(editMode))
+modeController.setValue(Mode.Draw).updateDisplay()
+modeController.onChange((value) => {
+	threeScene.updateMode(editMode[value])
+})
 
 const extrudeFolder = gui.addFolder("Extrude")
 const extrudeOptions = {
 	extrusionHeight: 5,
 }
-
-var drawModeController = gui
-	.add(threeScene, "drawMode", false)
-	.onChange((value) => {
-		threeScene.drawMode = value
-		// Update move mode to false when draw mode is enabled
-		if (value) {
-			threeScene.moveMode = false
-			moveModeController.updateDisplay()
-		}
-	})
-var moveModeController = gui
-	.add(threeScene, "moveMode", false)
-	.onChange((value) => {
-		threeScene.moveMode = value
-		// Update drawmode to false when move mode is enabled
-		if (value) {
-			threeScene.drawMode = false
-			drawModeController.updateDisplay()
-		}
-	})
-
 extrudeFolder.add(extrudeOptions, "extrusionHeight", 0, 5).onChange((value) => {
 	// Update the extrusion height of the selected object
-	console.log(threeScene.selectedObject)
-	if (threeScene.selectedObject) {
-		threeScene.selectedObject.scale.y = value
-	}
+	threeScene.extrudeSelectedShape(value)
 })
 
 document.body.appendChild(threeScene.renderer.domElement)
-
-threeScene.addHorizontalPlane()
-
-threeScene.renderer.setAnimationLoop(() => {
-	threeScene.renderer.render(threeScene.scene, threeScene.camera)
-})
-
 window.addEventListener("pointermove", (e) => {
 	threeScene.onPointerMove(e)
 })
-
 window.addEventListener("mousedown", threeScene.onPointerDown.bind(threeScene))
 window.addEventListener("pointerup", threeScene.onPointerUp.bind(threeScene))
-
 window.addEventListener("resize", () => {
 	threeScene.camera.aspect = window.innerWidth / window.innerHeight
 	threeScene.camera.updateProjectionMatrix()
--- src/initScene.ts
+++ src/initScene.ts
@@ -1,29 +1,25 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/Addons.js"
+import { Mode } from "."
 
 export class ThreeScene {
-	public scene: THREE.Scene
-	public camera: THREE.PerspectiveCamera
-	public light: THREE.Light
-	public renderer: THREE.WebGLRenderer
-	public raycaster: THREE.Raycaster
-	public controls: OrbitControls
-	public mousePosition: THREE.Vector2
-	public drawMode: boolean
-	public moveMode: boolean
-	public vertexEditMode: boolean
-	public extrudedObjects: THREE.Mesh[]
-	public selectedObject: THREE.Mesh | null
-	public extrusionHeight: number
-	public drawingLine: THREE.Line | null
-	public axisHelper: THREE.AxesHelper
-	public objects: THREE.Object3D[]
-	public clickCount: number
-	public controlPoints: THREE.Vector3[]
-	public plane: THREE.Mesh
-	public planeId: number
-	public offset: THREE.Vector3
-	public intersects: THREE.Intersection[]
+	private scene: THREE.Scene
+	private camera: THREE.PerspectiveCamera
+	private light: THREE.Light
+	private renderer: THREE.WebGLRenderer
+	private raycaster: THREE.Raycaster
+	private controls: OrbitControls
+	private mousePosition: THREE.Vector2
+	private mode: Mode
+	private selectedObject: THREE.Mesh | null
+	private axisHelper: THREE.AxesHelper
+	private axisHelperId: number
+	private controlPoints: THREE.Vector2[]
+	private plane: THREE.Mesh
+	private planeId: number
+	private offset: THREE.Vector3
+	private intersects: THREE.Intersection[]
+
 	constructor() {
 		this.scene = new THREE.Scene()
 		this.camera = new THREE.PerspectiveCamera(
@@ -35,32 +31,29 @@ export class ThreeScene {
 		this.light = new THREE.DirectionalLight(0xffffff, 2)
 		this.renderer = new THREE.WebGLRenderer()
 		this.renderer.setSize(window.innerWidth, window.innerHeight)
-		document.body.appendChild(this.renderer.domElement)
 		this.raycaster = new THREE.Raycaster()
 		this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 		this.mousePosition = new THREE.Vector2()
-		this.drawMode = false
-		this.moveMode = false
-		this.vertexEditMode = false
-		this.extrudedObjects = []
+		this.mode = Mode.Draw
 		this.selectedObject = null
-		this.extrusionHeight = 5
-		this.drawingLine = null
 		this.axisHelper = new THREE.AxesHelper(20)
 		this.controls.update()
 		this.camera.position.set(2, 3, 20)
-		// this.camera.rotateY(Math.PI / 4)
-		// this.camera.rotateX(-Math.PI / 4)
-		this.objects = []
 		this.plane = this.addHorizontalPlane()
 		this.planeId = this.plane.id
-		this.clickCount = 0
+		this.axisHelperId = this.axisHelper.id
 		this.controlPoints = []
 		this.offset = new THREE.Vector3()
 		this.intersects = []
 		this.setupCamera()
-		this.objects.push(this.plane)
 		this.addObjectsToScene(this.axisHelper, this.camera, this.light, this.plane)
+		this.setAnimationLoopForRenderer()
+	}
+
+	setAnimationLoopForRenderer() {
+		this.renderer.setAnimationLoop(() => {
+			this.renderer.render(this.scene, this.camera)
+		})
 	}
 
 	setupCamera() {
@@ -84,16 +77,28 @@ export class ThreeScene {
 		return plane
 	}
 
+	updateMode(mode: Mode) {
+		this.mode = mode
+	}
+
+	extrudeSelectedShape(height: number) {
+		console.log(this.selectedObject)
+		if (this.selectedObject) {
+			this.selectedObject.scale.y = height
+		}
+	}
+
 	// Implement drag and drop functionality on mouse drag for selected object
 	onPointerMove(e: MouseEvent) {
 		this.mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1
 		this.mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1
 		this.raycaster.setFromCamera(this.mousePosition, this.camera)
-		this.intersects = this.raycaster.intersectObjects(this.objects)
+		this.intersects = this.raycaster.intersectObjects(this.scene.children)
 		if (
-			this.moveMode &&
+			this.mode === Mode.Move &&
 			this.selectedObject &&
-			this.selectedObject.id !== this.planeId
+			this.selectedObject.id !== this.planeId &&
+			this.selectedObject.id !== this.axisHelperId
 		) {
 			this.controls.enabled = false
 			this.selectedObject.position.copy(
@@ -104,67 +109,57 @@ export class ThreeScene {
 	}
 
 	onPointerUp(event: MouseEvent) {
-		if (this.moveMode || this.drawMode) {
+		if (this.mode === Mode.Move || this.mode === Mode.Draw) {
 			this.selectedObject = null
-			this.controls.enabled = true
 		}
+		this.controls.enabled = true
 	}
 
 	onPointerDown(event: MouseEvent) {
-		this.mousePosition.x = (event.clientX / window.innerWidth) * 2 - 1
-		this.mousePosition.y = -(event.clientY / window.innerHeight) * 2 + 1
-		this.raycaster.setFromCamera(this.mousePosition, this.camera)
-		this.intersects = this.raycaster.intersectObjects(this.objects)
-		if (this.intersects.length > 0) {
-			this.selectedObject = this.intersects[0].object as THREE.Mesh
-			if (event.buttons === 1) {
-				if (this.drawMode) {
-					// To ensure that the user is not clicking on the same point or any other object in the scene
-					if (this.intersects.length === 1) {
-						var cp = new THREE.Mesh(
-							new THREE.SphereGeometry(0.1, 80, 80),
-							new THREE.MeshBasicMaterial({ color: "red" })
-						)
-						cp.position.copy(this.intersects[0].point)
-						this.controlPoints.push(this.intersects[0].point.clone())
-						this.scene.add(cp)
-					}
-				}
-				if (this.moveMode) {
-					this.selectedObject = this.intersects[0].object as THREE.Mesh
-					this.offset
-						.copy(this.intersects[0].point)
-						.sub(this.selectedObject.position)
-				}
-			} else if (event.buttons === 2) {
-				if (this.drawMode) {
-					this.selectedObject = null
-					console.log(this.controlPoints)
-					const shape = new THREE.Shape()
-					for (let i = 0; i < this.controlPoints.length; i++) {
-						if (i === 0) {
-							shape.moveTo(this.controlPoints[i].x, -this.controlPoints[i].z)
-						}
-						shape.lineTo(this.controlPoints[i].x, -this.controlPoints[i].z)
-					}
-					// add shape to scene
-					const extrudeSettings = {
-						steps: 10,
-						depth: 1,
-						amount: 2,
-						bevelEnabled: false,
-					}
-					const extrudeGeom = new THREE.ExtrudeGeometry(shape, extrudeSettings)
-					extrudeGeom.rotateX(-Math.PI / 2)
-					const wall = new THREE.Mesh(
-						extrudeGeom,
-						new THREE.MeshStandardMaterial({
-							color: "gray",
-						})
+		if (this.intersects.length === 0) {
+			return
+		}
+		this.selectedObject = this.intersects[0].object as THREE.Mesh
+		if (event.buttons === 1) {
+			if (this.mode === Mode.Draw) {
+				// To ensure that the user is not clicking on the same point or any other object in the scene
+				if (this.intersects.length === 1) {
+					var cp = new THREE.Mesh(
+						new THREE.SphereGeometry(0.1, 80, 80),
+						new THREE.MeshBasicMaterial({ color: "red" })
 					)
-					this.scene.add(wall)
-					this.objects.push(wall)
+					const pointXZPos = new THREE.Vector2(
+						this.intersects[0].point.x,
+						-1 * this.intersects[0].point.z
+					)
+					cp.position.copy(this.intersects[0].point)
+					this.controlPoints.push(pointXZPos)
+					this.scene.add(cp)
 				}
+			}
+			if (this.mode === Mode.Move) {
+				this.selectedObject = this.intersects[0].object as THREE.Mesh
+				this.offset
+					.copy(this.intersects[0].point)
+					.sub(this.selectedObject.position)
+			}
+		} else if (event.buttons === 2) {
+			if (this.mode === Mode.Draw) {
+				this.selectedObject = null
+				const shape = new THREE.Shape(this.controlPoints)
+				const extrudeSettings = {
+					steps: 10,
+					depth: 1,
+					amount: 2,
+					bevelEnabled: false,
+				}
+				const extrudeGeom = new THREE.ExtrudeGeometry(shape, extrudeSettings)
+				extrudeGeom.rotateX(-Math.PI / 2)
+				const wall = new THREE.Mesh(
+					extrudeGeom,
+					new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+				)
+				this.scene.add(wall)
 			}
 		}
 	}
